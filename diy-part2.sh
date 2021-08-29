e995069c27fa7ae56aabc7a0851a77849b95361a
roacn <84313163+roacn@users.noreply.github.com>
2021-08-29 22:53:26 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -115,7 +115,7 @@ rm -rf ./small/v2ray-plugin
 rm -rf ./small/xray-core
 rm -rf ./small/xray-plugin
 
-svn co https://github.com/roacn/Actions-OpenWrt-Lede/trunk/root/usr/share/passwall/rules rules
+svn co https://github.com/roacn/Actions-OpenWrt-Lede/trunk/files/root/usr/share/passwall/rules rules
 #cp -r ./rules/* ./openwrt-packages/luci-app-passwall/root/usr/share/passwall/rules
 cp -r ./rules/direct_ip ./openwrt-packages/luci-app-passwall/root/usr/share/passwall/rules/direct_ip
 cp -r ./rules/direct_host ./openwrt-packages/luci-app-passwall/root/usr/share/passwall/rules/direct_host
