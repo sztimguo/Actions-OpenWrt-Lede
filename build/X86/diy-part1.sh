588668e7b99ca7008a10f05e93f94bb04a65e9cc
roacn <84313163+roacn@users.noreply.github.com>
2022-01-17 20:42:10 +0800

    Update diy-part1.sh

--- build/X86/diy-part1.sh
+++ build/X86/diy-part1.sh
@@ -18,7 +18,7 @@
 
 # 修改IP项的EOF于EOF之间请不要插入其他扩展代码，可以删除或注释里面原本的代码
 cat >$NETIP <<-EOF
-uci set network.lan.ipaddr='192.168.1.5'                                    # IPv4 地址(openwrt后台地址)
+uci set network.lan.ipaddr='192.168.1.2'                                    # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                             # IPv4 子网掩码
 uci set network.lan.gateway='192.168.1.1'                                 # IPv4 网关
 uci set network.lan.broadcast='192.168.1.255'                           # IPv4 广播
@@ -27,7 +27,7 @@ uci set network.lan.delegate='0'                                                
 uci commit network                                                                    # 不要删除跟注释,除非上面全部删除或注释掉了
 uci set dhcp.lan.ignore='1'                                                          # 关闭DHCP功能
 uci commit dhcp                                                                          # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
-uci set system.@system[0].hostname='OpenWrt-MI'               # 修改主机名称为OpenWrt-MI
+uci set system.@system[0].hostname='OpenWrtX'               # 修改主机名称为OpenWrt-MI
 EOF
 
 cat >$WEBWEB <<-EOF
